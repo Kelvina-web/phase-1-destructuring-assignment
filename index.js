06405258fbf2640069bae5657fc1ae252f3816cf
Kelvina <Kelvina-web.com>
2025-03-04 23:11:35 +0300

    Adding files

--- index.js
+++ index.js
@@ -28,21 +28,58 @@ const nestedMuppet = {
 // Strings
 
 // 1. Use destructuring to assign appropriate variables based on the sounds animals make.
+const animalSounds = {
+  moo: 'cow',
+  neigh: 'horse',
+  baa: 'sheep',
+  oink: 'pig',
+  cluck: 'chicken'
+};
+const { moo, neigh, baa, oink, cluck } = animalSounds;
 
 // 2. Bolt the horse wandered off, so just give us four animals, and let's name them bessie, dolly, babe, and little.
+const traditionalAnimals = {
+  bessie: 'cow',
+  dolly: 'sheep',
+  babe: 'pig',
+  little: 'chicken'
+};
+const { bessie, dolly, babe, little } = traditionalAnimals;
 
 // 3. Little the chicken had to go back to the coop, so now we're left with three. Let's use color variables of blackAndWhite, black, and pink.
+const animalColors = {
+  blackAndWhite: 'cow',
+  black: 'sheep',
+  pink: 'pig'
+};
+const { blackAndWhite, black, pink } = animalColors;
 
 // Arrays
 
 // 4. Use destructuring to assign appropriate variables using the color names.
+const rainbowColors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
+const [red, orange, yellow, green, blue, indigo, violet] = rainbowColors;
 
-// 5. Some people have a really hard time picking out indigo, so let's leave that one out, using the first letter of each color as the variable names.
+// 5. Declare six rainbow color variables using initials
+const [r, o, y, g, b, i, v] = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
 
 // 6. But wait! Indigo is now feeling *super* left out. Let's only assign indigo using indg. 
+const [, , , , , indg] = rainbowColors;
 
 // Objects
 
-// 7. Use destructuring to assign all variables using the keys as the variable names
+// 7. Assign all appropriate variables using the keys as the variable names
+const muppet = {
+  muppetName: 'Kermit',
+  color: 'green',
+  song1: 'Rainbow Connection',
+  song2: 'Bein’ Green',
+  song3: 'Happy Feet',
+  song4: 'The Muppet Show Theme',
+  job: 'Host of The Muppet Show',
+  partner: 'Miss Piggy'
+};
+const { muppetName, color, song1, song2, song3, song4, job, partner } = muppet;
 
-// 8. Use destructuring to assign songs 2 and 4, and Kermit's job and partner
+// 8. Assign songs 2 and 4, and Kermit's job and partner
+const { song2, song4, job: kermitJob, partner: kermitPartner } = muppet;
